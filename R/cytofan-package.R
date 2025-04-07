bd883038889f4ac9cce44898fd315950c8f29f9f
Yann Abraham <yannabraham@mac.tailb93c94.ts.net>
2025-04-07 10:11:01 +0200

    changed the outdated docType

--- R/cytofan-package.R
+++ R/cytofan-package.R
@@ -2,7 +2,6 @@
 #'
 #' For reference see \href{https://www.bankofengland.co.uk/quarterly-bulletin/1998/q1/the-inflation-report-projections-understanding-the-fan-chart}{Britton, E.; Fisher, P. & J. Whitley (1998) The Inflation Report Projections: Understanding the Fan Chart}.
 #'
-#' @docType package
 #' @name cytofan
 #' @import ggplot2
-NULL
+"_PACKAGE"
